7f292cce443a696923d00f2ed67897dfd7ee9a7e
natatat <natuduwela@gmail.com>
2015-04-22 15:28:00 -0700

    first commit, setting up the js object

---
+++ human-js.js
@@ -0,0 +1,9 @@
+var HumanJavaScript = function () {
+    var url = "http://read.humanjavascript.com/":
+
+    // 1. Foreword by Jan Lehnardt
+
+    // 2. Intro
+
+    // 3. Client or server? Go big or go home.
+};
